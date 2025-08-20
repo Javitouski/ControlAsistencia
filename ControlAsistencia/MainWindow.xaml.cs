0b690f80b58b135c860f3f1399fd55e7a0bfa07d
Javitouski <cronoscr933@gmail.com>
2025-08-19 20:46:48 -0400

    Se agregan dos txtBox más boton LogIn con verificacion de credencial SOLAMENTE DE ADMINISTRADOR por el momento.
    Se creo AdminWindow.

--- ControlAsistencia/MainWindow.xaml.cs
+++ ControlAsistencia/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ControlAsistencia.Data;
+using ControlAsistencia.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,32 @@ namespace ControlAsistencia
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnLog_Click(object sender, RoutedEventArgs e)
+        {
+            string correo = txtCorreo.Text;
+            string password = txtPassword.Text;
+
+            //Se conecta a la base de datos
+            using var db = new AsistenciaDbContext();
+
+            //Buscar usuario con el correo y contraseña ingresados más el rol de administrador
+            var usuario = db.Usuarios
+                .FirstOrDefault(u => u.Correo == correo && u.Rol == Rol.ADMIN);
+
+            if (usuario != null && BCrypt.Net.BCrypt.Verify(password, usuario.HashPassword))
+            {
+                // Si el usuario existe y es admin, abre la ventana de administración
+                var adminWin = new AdminWindow();
+                adminWin.Show();
+                this.Hide();
+            }
+            else
+            {
+                // Si no existe, muestra mensaje de error
+                MessageBox.Show("Credenciales incorrectas o no eres administrador.");
+            }
         }
     }
 }
